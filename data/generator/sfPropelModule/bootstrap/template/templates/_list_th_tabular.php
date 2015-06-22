e2e3f721021c3320f393ec65a63dd199f193fbf4
rozwell <rozwell69@gmail.com>
2015-06-22 04:49:27 +0200

    "is_sortable: false" can now be used to disable sorting

--- data/generator/sfPropelModule/bootstrap/template/templates/_list_th_tabular.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_list_th_tabular.php
@@ -1,7 +1,7 @@
 <?php foreach ($this->configuration->getValue('list.display') as $name => $field): ?>
     [?php slot('sf_admin.current_header') ?]
     <th class="sf_admin_<?php echo strtolower($field->getType()) ?> sf_admin_list_th_<?php echo $name ?>">
-        <?php if ($field->isReal() || $field->getConfig('is_sortable', false, false)): ?>
+        <?php if (($field->isReal() || $field->getConfig('is_sortable', false, false)) && $field->getConfig('is_sortable', true, false) === true): ?>
             [?php if ('<?php echo $name ?>' == $sort[0]): ?]
                 [?php echo link_to(__('<?php echo $field->getConfig('label', '', true); ?>', array(), '<?php echo $this->getI18nCatalogue() ?>') . ' <i class="fa fa-angle-' . ($sort[1] == 'asc' ? 'up' : 'down') . '"></i>', '@<?php echo $this->getUrlForAction('list') ?>', array('query_string' => 'sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))); ?]
             [?php else: ?]
