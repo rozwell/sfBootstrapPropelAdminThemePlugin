6c61e972fa669d916dd3ef9a05f96283b3fb0f82
rozwell <rozwell69@gmail.com>
2015-11-02 21:42:39 +0100

    Always display pagination row on top of the table

--- data/generator/sfPropelModule/bootstrap/template/templates/_list_pagination_top.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_list_pagination_top.php
@@ -1,3 +1,1 @@
-[?php if ($pager->haveToPaginate()): ?]
-    [?php include_partial('<?php echo $this->getModuleName() ?>/list_pagination_bottom', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)); ?]
-[?php endif; ?]
+[?php include_partial('<?php echo $this->getModuleName() ?>/list_pagination_bottom', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)); ?]
